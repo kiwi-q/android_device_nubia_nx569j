524e4319264ffe9b255ebef7c05717d72da76065
jsbeyond <67664518@gmail.com>
2019-09-30 12:08:00 +0800

    fix

--- device.mk
+++ device.mk
@@ -16,6 +16,7 @@
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Screen density
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_AAPT_CONFIG := normal
